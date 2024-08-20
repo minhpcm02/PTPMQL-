375e6bac01de103fab317b32db969108a54e0f28
minhpcm02 <mngocminh73@gmail.com>
2024-08-20 13:31:05 +0700

    creatProject

--- DemoMvc/Controllers/HomeController.cs
+++ DemoMvc/Controllers/HomeController.cs
@@ -17,6 +17,13 @@ public class HomeController : Controller
     {
         return View();
     }
+    [HttpPost]
+    public IActionResult Index(string FullName )
+    {
+        string strOutput = "helo chao cau" + "-" + FullName;
+        ViewBag.hghh = strOutput;
+        return View();
+    }
 
     public IActionResult Privacy()
     {
